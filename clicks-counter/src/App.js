dc5f015f622ac2e5b67c36c00ce12df4d34a6c23
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-13 07:49:07 -0300

    adjusted logo size

--- clicks-counter/src/App.js
+++ clicks-counter/src/App.js
@@ -4,10 +4,13 @@ import logo from './images/logo.png'
 function App() {
   return (
     <div className='App'>
-      <div className='logo-contenedor'>
+      <div className='logo-container'>
         <img className='logo'
         src={logo}
         alt='logo' />
+      </div>
+      <div className='main-container'>
+        
       </div>
     </div>
   );
