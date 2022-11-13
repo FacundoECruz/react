c18fe1943be60463d273777201fb1840079e2380
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-13 07:34:03 -0300

    logo imported

---
+++ clicks-counter/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './images/logo.png'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <img className='logo'
+        src={logo}
+        alt='logo' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
