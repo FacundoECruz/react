7b87937c9ff87708c703521c659336aca4105b91
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-13 09:10:46 -0300

    title replaced by logo

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from 'react';
+import logo from './images/logo.jpg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <div className='title-container'>
-        <h1>Calculadora</h1>
+        <img
+          src={logo}
+          className='logo'
+          alt='logo' />
       </div>
     </div>
   );
