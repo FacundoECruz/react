0368cd4be715f229f4b12a48caaa4a0ce9ae2144
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-22 19:00:29 -0300

    icon added

--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../stylesheets/Task.css'
+import { IoTrashOutline } from 'react-icons/ai';
 
-function Task({ text }) {
+function Task({ text, complete }) {
   return (
     <div className='task-container'>
       <div className='task-text'>
         {text}
       </div>
-      <div className='task-icon'>
-        Eliminar
+      <div className='task-icon-container'>
+        <IoTrashOutline className='task-icon'/>
       </div>
     </div>    
   )
