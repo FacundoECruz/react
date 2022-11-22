e172c1c439e1023945e925f44d9f07a6f75f6636
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-22 17:25:03 -0300

    logo added

--- tasks/src/App.js
+++ tasks/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.svg'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-container'>
+        <img 
+          src={logo} 
+          className='logo' />
+      </div>
     </div>
   );
 }
