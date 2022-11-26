9f5c80be0a2522010635680304c25d1d9402a4b3
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-26 12:36:45 -0300

    color gradient for tasks

--- tasks/src/components/ListTask.js
+++ tasks/src/components/ListTask.js
@@ -8,6 +8,7 @@ function ListTask() {
   const [tasks, setTask] = useState([]);
 
   const addTask = task => {
+    console.log(task)
     if (task.text.trim()) {
       task.text = task.text.trim();
       const updatedTasks = [task, ...tasks];
@@ -22,6 +23,8 @@ function ListTask() {
         {
           tasks.map((task) => 
             <Task 
+              key={task.id}
+              id={task.id}
               text={task.text}
               completed={task.completed} />
           )
