5d0c08cc5d2fabef6468cd6bca8c845cbde245f1
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-28 14:54:50 -0300

    moved form to separate file

--- altisima/altisima/src/App.js
+++ altisima/altisima/src/App.js
@@ -1,85 +1,12 @@
 import "./App.css";
-import { useState } from "react";
-import logo from "./images/logo.jpg";
-import "./stylesheets/Input.css";
-import { v4 as uuidv4 } from "uuid";
-import Player from "./components/Player";
+import Form from "./components/Form"
 
-function Input({ name, onChange, onSubmit }) {
-  return (
-    <div className="main-input-container">
-      <div className="logo-container">
-        <img src={logo} alt="..." />
-      </div>
-      <div className="input-container">
-        <h2>Agregar jugadores</h2>
-        <form className="player-form" onSubmit={onSubmit}>
-          <input
-            className="player-input"
-            type="text"
-            placeholder="Nombre del jugador"
-            name="name"
-            value={name}
-            onChange={onChange}
-          />
-          <button className="input-button">Agregar</button>
-        </form>
-      </div>
-    </div>
-  );
-}
-
-function List({ players, setPlayers }) {
-  
-  const deletePlayer = (id) => {
-    const updatedPlayers = players.filter((player) => player.id !== id);
-    setPlayers(updatedPlayers);
-  };
-
-  return (
-    <>
-      <div className="list-container">
-        {players.map((player) => (
-          <Player
-            key={player.id}
-            id={player.id}
-            name={player.name}
-            deletePlayer={deletePlayer}
-          />
-        ))}
-      </div>
-    </>
-  );
-}
 
 function App() {
-  const [name, setName] = useState("");
-  const [players, setPlayers] = useState([]);
-
-  const addPlayer = (e) => {
-    e.preventDefault();
-
-    const newPlayer = {
-      id: uuidv4(),
-      name: name,
-    };
-
-    if (newPlayer.name.trim()) {
-      newPlayer.name = newPlayer.name.trim();
-      const updatedPlayers = [...players, newPlayer];
-      setPlayers(updatedPlayers);
-      setName("");
-    }
-  };
-
+  
   return (
     <div className="App">
-      <Input
-        name={name}
-        onChange={(e) => setName(e.target.value)}
-        onSubmit={addPlayer}
-      />
-      <List players={players} setPlayers={setPlayers}/>
+      <Form />
     </div>
   );
 }
---
+++ altisima/altisima/src/components/Form.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import logo from "../images/logo.jpg";
+import "../stylesheets/Input.css";
+import { v4 as uuidv4 } from "uuid";
+import Player from "../components/Player";
+
+function Input({ name, onChange, onSubmit }) {
+  return (
+    <div className="main-input-container">
+      <div className="logo-container">
+        <img src={logo} alt="..." />
+      </div>
+      <div className="input-container">
+        <h2>Agregar jugadores</h2>
+        <form className="player-form" onSubmit={onSubmit} autocomplete="off">
+          <input
+            className="player-input"
+            type="text"
+            placeholder="Nombre del jugador"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
+          <button className="input-button">Agregar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function List({ players, setPlayers }) {
+  const deletePlayer = (id) => {
+    const updatedPlayers = players.filter((player) => player.id !== id);
+    setPlayers(updatedPlayers);
+  };
+
+  return (
+    <>
+      <div className="list-container">
+        {players.map((player) => (
+          <Player
+            key={player.id}
+            id={player.id}
+            name={player.name}
+            deletePlayer={deletePlayer}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Form() {
+  const [name, setName] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  const addPlayer = (e) => {
+    e.preventDefault();
+
+    const newPlayer = {
+      id: uuidv4(),
+      name: name,
+    };
+
+    if (newPlayer.name.trim()) {
+      newPlayer.name = newPlayer.name.trim();
+      const updatedPlayers = [...players, newPlayer];
+      setPlayers(updatedPlayers);
+      setName("");
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <Input
+        name={name}
+        onChange={(e) => setName(e.target.value)}
+        onSubmit={addPlayer}
+      />
+      <List players={players} setPlayers={setPlayers} />
+    </div>
+  );
+}
+
+export default Form;
