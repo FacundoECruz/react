d85fbfa62eab4d433fce023eccef63c49d571073
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-28 14:38:48 -0300

    solved lifting state issue

--- altisima/altisima/src/App.js
+++ altisima/altisima/src/App.js
@@ -1,90 +1,77 @@
-import './App.css';
-import { useState } from 'react'
-import logo from './images/logo.jpg'
-import './stylesheets/Input.css'
-import { v4 as uuidv4 } from 'uuid'
-import Player from './components/Player'
+import "./App.css";
+import { useState } from "react";
+import logo from "./images/logo.jpg";
+import "./stylesheets/Input.css";
+import { v4 as uuidv4 } from "uuid";
+import Player from "./components/Player";
 
-function Input({onSubmit}) {
-
-  const initialValue = ''
-
-  const [text, setText] = useState(initialValue);
-
-  const changeHandler = e => {
-    setText(e.target.value);
-  }
-
-  const sendHandler = e => {
-    e.preventDefault();
-
-    const newPlayer = {
-      id: uuidv4(),
-      name: text
-    }
-    onSubmit(newPlayer);
-  }
-
+function Input({ name, onChange, onSubmit }) {
+ 
   return (
-    <div className='main-input-container'>
-      <div className='logo-container'>
-        <img src={logo} alt='...' />
+    <div className="main-input-container">
+      <div className="logo-container">
+        <img src={logo} alt="..." />
       </div>
-      <div className='input-container'>
+      <div className="input-container">
         <h2>Agregar jugadores</h2>
-        <form 
-        className='player-form'
-        onSubmit={sendHandler}>
+        <form className="player-form" onSubmit={onSubmit}>
           <input
-            className='player-input'
-            type='text'
-            placeholder='Nombre del jugador'
-            name='name'
-            onChange={changeHandler} />
-          <button className='input-button'>
-            Agregar
-          </button>
+            className="player-input"
+            type="text"
+            placeholder="Nombre del jugador"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
+          <button className="input-button">Agregar</button>
         </form>
       </div>
     </div>
-  )
-}
-
-
-function List() {
-
-  const [players, setPlayers] = useState([]);
-
-  const addPlayer = player => {
-      if (player.name.trim()) {
-          player.name = player.name.trim();
-          const updatedPlayers = [...players, player];
-          setPlayers(updatedPlayers);
-      }
-  }
-
-  return (
-      <>
-      <Input onSubmit={addPlayer} />
-        <div className='list-container'>
-          {
-              players.map((player) => 
-              <Player
-                  key={player.id}
-                  id={player.id}
-                  name={player.name} />
-              )
-          }
-        </div>
-      </>
   );
 }
-  
+
+
+
+function List({players}) {
+  return (
+    <>
+      <div className="list-container">
+        {players.map((player) => (
+          <Player key={player.id} id={player.id} name={player.name} />
+        ))}
+      </div>
+    </>
+  );
+}
 
 function App() {
+  const [name, setName] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  const addPlayer = (e, player) => {
+    e.preventDefault()
+
+    const newPlayer = {
+      id: uuidv4(),
+      name: name
+    }
+
+    if (newPlayer.name.trim()) {
+      newPlayer.name = newPlayer.name.trim();
+      const updatedPlayers = [...players, newPlayer];
+      setPlayers(updatedPlayers);
+      setName("")
+    }
+  };
+
   return (
     <div className="App">
-      <List />
+      <Input
+        name={name}
+        onChange={(e) => setName(e.target.value)}
+        onSubmit={addPlayer}
+      />
+      <List players={players} />
     </div>
   );
 }
