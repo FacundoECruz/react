95db11a5e667afcfe620a048ce26fa244d3c59c2
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-01-26 12:03:13 -0300

    added some temporary styles

--- altisima/altisima/src/components/PlayerGrid.js
+++ altisima/altisima/src/components/PlayerGrid.js
@@ -14,14 +14,14 @@ function PlayerGrid({
       <h2 className="name">{state[index].name}</h2>
       <div className="buttons">
         <div className="bid-container">
-          <p className="bid-label" onClick={resetBid}>Apuesta</p>
-          <button id="bid" onClick={setBidState}>
+          <p className="label" onClick={resetBid}>Apuesta</p>
+          <button id="bid-button" onClick={setBidState}>
             {state[index].bid}
           </button>
         </div>
         <div className="lost-container">
-          <p className="lost-label" onClick={resetLost}>Pierde</p>
-          <button id="lost" onClick={setLoseState}>
+          <p className="label" onClick={resetLost}>Pierde</p>
+          <button id="lost-button" onClick={setLoseState}>
             {state[index].bidsLost}
           </button>
         </div>
