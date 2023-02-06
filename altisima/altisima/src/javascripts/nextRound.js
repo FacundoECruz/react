116c0d02c7f0b30468cd926b39d39c4e0a03f16a
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-02-06 08:10:59 -0300

    [wip] set context: added provider and reducer

---
+++ altisima/altisima/src/javascripts/nextRound.js
@@ -0,0 +1,15 @@
+function nextRound(tableState) {
+    const pointsForWin = 5;
+  
+    tableState.map(p => {
+      if (p.bidsLost === 0) {
+        p.score = p.score + pointsForWin + p.bid;
+      } else {
+        p.score = p.score - p.bidsLost;
+      }
+    })
+    return tableState;
+  }
+  
+  export default nextRound;
+  
