0ada5c42a15eb08f2437ab0d37bb25770fc873f8
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-30 16:06:20 -0300

    sending post request

--- vite-proyects/login_practice/__tests__/App.test.js
+++ vite-proyects/login_practice/__tests__/App.test.js
@@ -1,14 +1,33 @@
 import {render, screen} from '@testing-library/react'
 import App from '../src/App';
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from '@testing-library/user-event';
 
-test('displays form for email and password', () => {
-  render(<App />)
+const createFormData = () => {
+  return {
+    email: 'hola@gmail.com',
+    password: 'contrase'
+  }
+}
 
-  
+test('displays form for email and password', async () => {
+  const handleSubmit = jest.fn()
 
+  render(<App onSubmit={handleSubmit} />)
+
+  const {email, password} = createFormData()
+
+  const emailField = screen.getByLabelText(/email/i);
+  const passwordField = screen.getByLabelText(/password/i);
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+
+  await userEvent.type(emailField, email)
+  await userEvent.type(passwordField, password)
+  await userEvent.click(submitButton)
   screen.debug()
-  // const emailField = screen.getByLabelText(/email/i);
-  // const passwordField = screen.getByLabelText(/password/i);
-  // const submitButton = screen.getByRole("button", { name: /submit/i });
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    email,
+    password,
+  })
 })
