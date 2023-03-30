dc73b378c2f9e5676262351a5dda23a63a23ff4b
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-30 13:21:24 -0300

    displaying table

---
+++ vite-proyects/http_requests_practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UsersPage from "./pages/UsersPage";
+
+function App() {
+  return (
+    <div className="container">
+      <UsersPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-proyects/http_requests_practice/src/pages/UsersPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function UsersPage() {
+  return ( 
+    <div className='mt-5'>
+      <h1>Users</h1>
+      <hr />
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>Email</td>
+            <td>Name</td>
+            <td>Avatar</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UsersPage;
