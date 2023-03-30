0ff876dff55592b86c7a8d80e6c50a7e1e3f6e02
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-29 21:48:42 -0300

    added screen utility

--- vite-proyects/practice-vite/__tests__/App.test.js
+++ vite-proyects/practice-vite/__tests__/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import App from "../src/App";
-import {render, fireEvent} from "@testing-library/react"
+import {render, screen, fireEvent} from "@testing-library/react"
 import '@testing-library/jest-dom/extend-expect'
 
 test('renders App heading when the app is running', () => { 
   const {container} = render(<App />)
 
+  const increment = screen.getByRole('button', {name: '+'})
+  const decrement = screen.getByRole('button', {name: '-'})
   
-  const [decrement, increment] = container.querySelectorAll('button')
   const message = container.querySelector('h3')
   console.log(message.innerHTML)
   expect(message).toHaveTextContent('Count: 0')
