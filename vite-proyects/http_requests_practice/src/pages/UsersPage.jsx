023925052c98edb6e35b6c9c7b3b0920c6139880
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-30 14:13:20 -0300

    rendering users list

--- vite-proyects/http_requests_practice/src/pages/UsersPage.jsx
+++ vite-proyects/http_requests_practice/src/pages/UsersPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { usersApi } from "../api/userApi";
 
 function UsersPage() {
-  return ( 
-    <div className='mt-5'>
+  const [users, setUsers] = useState([]);
+
+  // console.log(users)
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await usersApi.get("https://reqres.in/api/users?page=2");
+      setUsers(res.data.data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="mt-5">
       <h1>Users</h1>
       <hr />
-      <table className='table'>
+      <table className="table">
         <thead>
           <tr>
             <th>ID</th>
@@ -15,12 +28,26 @@ function UsersPage() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>ID</td>
-            <td>Email</td>
-            <td>Name</td>
-            <td>Avatar</td>
-          </tr>
+          {users.map((u) => {
+            return (
+              <tr key={u.email}>
+                <td>{u.id}</td>
+                <td>{u.email}</td>
+                <td>
+                  {u.last_name}, {u.first_name}
+                </td>
+                <td>
+                  <img
+                    src={u.avatar}
+                    className="img-thumbnail"
+                    style={{
+                      width: 50,
+                    }}
+                  />
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
