5c007a8b9b984b6b41ed190c1234d496f8fcc337
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-30 11:05:07 -0300

    refactored login data

--- vite-proyects/practice-vite/__tests__/App.test.js
+++ vite-proyects/practice-vite/__tests__/App.test.js
@@ -1,41 +1,50 @@
-import React from 'react';
+import React from "react";
 import App from "../src/App";
-import Login from '../src/Login';
-import {render, screen} from "@testing-library/react"
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom/extend-expect'
+import Login from "../src/Login";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
 
-test('renders App heading when the app is running', async () => { 
-  render(<App />)
+test("renders App heading when the app is running", async () => {
+  render(<App />);
 
-  const increment = screen.getByRole('button', {name: '+'})
-  const decrement = screen.getByRole('button', {name: '-'})
-  const message = screen.getByText(/count/i)
+  const increment = screen.getByRole("button", { name: "+" });
+  const decrement = screen.getByRole("button", { name: "-" });
+  const message = screen.getByText(/count/i);
 
-  expect(message).toHaveTextContent('Count: 0')
-  
-  await userEvent.click(increment)
-  expect(message).toHaveTextContent('Count: 1')
+  expect(message).toHaveTextContent("Count: 0");
 
-  await userEvent.click(decrement)
-  expect(message).toHaveTextContent('Count: 0')
-})
+  await userEvent.click(increment);
+  expect(message).toHaveTextContent("Count: 1");
 
-test('submitting the form calls onSubmit with username and password', async () => {
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
-  render(<Login onSubmit={handleSubmit} />)
-  const usernameField = screen.getByLabelText(/username/i)
-  const passwordField = screen.getByLabelText(/password/i)
-  const submitButton = screen.getByRole('button', { name: /submit/i})
-  
-  await userEvent.type(usernameField, 'elfacu')
-  await userEvent.type(passwordField, 'gatorade')
-  await userEvent.click(submitButton)
+  await userEvent.click(decrement);
+  expect(message).toHaveTextContent("Count: 0");
+});
 
-  expect(submittedData).toEqual({
-    username: 'elfacu',
-    password: 'gatorade'
-  })
-})
+test("submitting the form calls onSubmit with username and password", async () => {
+  const handleSubmit = jest.fn();
+  const buildLoginForm = () => {
+    return {
+      username: "elfacu",
+      password: "gatorade",
+    };
+  };
 
+  render(<Login onSubmit={handleSubmit} />);
+
+  const {username, password} = buildLoginForm()
+
+  const usernameField = screen.getByLabelText(/username/i);
+  const passwordField = screen.getByLabelText(/password/i);
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+
+  await userEvent.type(usernameField, username);
+  await userEvent.type(passwordField, password);
+  await userEvent.click(submitButton);
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  });
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+});
