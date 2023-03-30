bb7d6d05df8446d86656105feabde5a4c5ddfba3
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-29 21:50:39 -0300

    taking message by screen

--- vite-proyects/practice-vite/__tests__/App.test.js
+++ vite-proyects/practice-vite/__tests__/App.test.js
@@ -4,13 +4,12 @@ import {render, screen, fireEvent} from "@testing-library/react"
 import '@testing-library/jest-dom/extend-expect'
 
 test('renders App heading when the app is running', () => { 
-  const {container} = render(<App />)
+  render(<App />)
 
   const increment = screen.getByRole('button', {name: '+'})
   const decrement = screen.getByRole('button', {name: '-'})
-  
-  const message = container.querySelector('h3')
-  console.log(message.innerHTML)
+  const message = screen.getByText(/count/i)
+
   expect(message).toHaveTextContent('Count: 0')
   
   fireEvent.click(increment)
