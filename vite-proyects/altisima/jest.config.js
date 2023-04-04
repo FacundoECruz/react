982b47a56ee9e40e7ac8cd9a52410408ddefc603
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-04 14:18:06 -0300

    setted jest and babel config

--- vite-proyects/altisima/jest.config.js
+++ vite-proyects/altisima/jest.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  moduleNameMapper: {
-    "\\.(css|less|scss)$": "identity-obj-proxy",
-  },
-  setupFilesAfterEnv: ['@testing-library/react'],
-  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-  },
-  testEnvironment: "node",
-};
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ["@testing-library/jest-dom", "./jest.setup.js"],}
