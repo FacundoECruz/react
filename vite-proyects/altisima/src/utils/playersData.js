00d70b9e59745267c034864531e9f0ebce2d6f57
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-04-05 15:19:11 -0300

    rendering player grids

---
+++ vite-proyects/altisima/src/utils/playersData.js
@@ -0,0 +1,28 @@
+const playersData = [
+  {
+    name: "Antone",
+    score: 26,
+    bet: 0,
+    lost: 0,
+  },
+  {
+    name: "Bren",
+    score: 12,
+    bet: 0,
+    lost: 0,
+  },
+  {
+    name: "Fonzo",
+    score: 20,
+    bet: 0,
+    lost: 0,
+  },
+  {
+    name: "Chueco",
+    score: 15,
+    bet: 0,
+    lost: 0,
+  },
+]
+
+export default playersData;
